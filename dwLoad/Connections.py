d175ee836619d186b54f68eb1b20d90b66172895
Chris Contino <samsquanch516@users.noreply.github.com>
2018-11-12 16:10:31 -0500

    start adding connectors

--- dwLoad/Connections.py
+++ dwLoad/Connections.py
@@ -1,7 +1,16 @@
+import psycopg2, MySQLdb, datetime
+
+
 class Connections:
 
     def __init__(self):
         self.is_connected = True
+        self.dw_host = ""
+        self.dw_pass = ""
+        self.dw_user = ""
+        self.dw_name = ""
+
+        connect_dw()
 
     def get_is_connected(self):
         if self.is_connected:
@@ -9,3 +18,12 @@ class Connections:
         else:
             print("DB is NOT Connected")
 
+    def connect_dw(self):
+        try:
+            print str(datetime.datetime.now()) + " - Attempting to connect to the Data Warehouse...."
+            psycopg2.connect(dbname=dw_name, user=dw_user, password=dw_pass,
+                                                       host=dw_host,
+                                                       port="5439")
+            print str(datetime.datetime.now()) + " - Success!"
+        except e as Exception:
+            print (str(datetime.datetime.now()) + " - Failed to connect!")
