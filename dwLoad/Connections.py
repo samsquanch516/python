eee06e23c4924fe47d2e7cbd6b32dd7d071c19ab
Chris Contino <samsquanch516@users.noreply.github.com>
2018-11-12 19:46:16 -0500

    updates

--- dwLoad/Connections.py
+++ dwLoad/Connections.py
@@ -1,18 +1,29 @@
-import psycopg2, datetime
+import psycopg2, datetime, MySQLdb
 
 
 class Connections:
 
     def __init__(self):
-        self.is_connected = True
+        self.is_connected_dw = False
+        self.is_connected_db = False
         self.dw_host = ""
         self.dw_pass = ""
         self.dw_user = ""
-        self.dw_name = ""
-        self.dw_connection = self.connect_dw()
+        self.dw_name = "reporting"
 
-    def get_is_connected(self):
-        return self.is_connected
+        self.db_host = "vox"
+        self.db_pass = ""
+        self.db_user = ""
+        self.db_name = "reporting"
+
+        self.dw_connection = self.connect_dw()
+        self.db_connection = self.connect_db()
+
+    def get_is_dw_connected(self):
+        return self.is_connected_dw
+
+    def get_is_db_connected(self):
+        return self.is_connected_db
 
     def connect_dw(self):
         try:
@@ -21,21 +32,52 @@ class Connections:
                                                        host=self.dw_host,
                                                        port="5439")
             print str(datetime.datetime.now()) + " - Success!"
+            self.is_connected_dw = True
             return c
         except Exception as e:
             print(str(datetime.datetime.now()) + " - Failed to connect!")
             print(e)
 
+    def connect_db(self):
+        # Trying to connect
+        try:
+            print str(datetime.datetime.now()) + " - Attempting to connect to the Database...."
+            d = MySQLdb.connect(self.db_host, self.db_user, self.db_pass, self.db_name)
+            print str(datetime.datetime.now()) + " - Success!"
+            self.is_connected_db = True
+            return d
+        except Exception as e:
+            print("Can't connect to database!")
+            print(e)
+
     def disconnect_dw(self):
         try:
             self.dw_connection.close()
+            self.is_connected_dw = False
+            print("Disconnected from DW")
+        except Exception as e:
+            print(e)
+
+    def disconnect_db(self):
+        try:
+            self.db_connection.close()
+            self.is_connected_db = False
+            print("Disconnected from DB")
         except Exception as e:
             print(e)
 
     def query_dw(self, query):
-        if self.is_connected:
+        if self.is_connected_dw:
             cur = self.dw_connection.cursor()
             cur.execute(query)
-            print(cur.fetchall())
+            print(cur.fetchone())
+        else:
+            print("Not Connected to DW")
+
+    def query_db(self, query):
+        if self.is_connected_db:
+            cursor = self.db_connection.cursor()
+            cursor.execute(query)
+            print(cursor.fetchone())
         else:
             print("Not Connected to DW")
